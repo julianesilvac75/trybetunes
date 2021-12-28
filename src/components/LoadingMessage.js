870c9c385627df7dbf5d46ce0b4d6a0e7883a568
Juliane Cardoso <julianesilvac75@gmail.com>
2021-12-28 09:14:56 -0300

    styles: estiliza componente de loading

--- src/components/LoadingMessage.js
+++ src/components/LoadingMessage.js
@@ -1,10 +1,27 @@
 import React, { Component } from 'react';
+import '../styles/LoadingMessage.css';
 
 class LoadingMessage extends Component {
   render() {
     return (
       <div>
-        <span>Carregando...</span>
+        <div className="lds-default">
+          <div />
+          <div />
+          <div />
+          <div />
+          <div />
+          <div />
+          <div />
+          <div />
+          <div />
+          <div />
+          <div />
+          <div />
+        </div>
+
+        {/* Loader reference: https://loading.io/css/ */}
+        <span className="loading">Carregando...</span>
       </div>
     );
   }
